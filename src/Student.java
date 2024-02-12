846eea0d63bba104f4b6dc674a2024ac3ee5644a
ijmrodriguez <ijonmrodriguez@gmail.com>
2024-02-12 13:19:54 -0800

    Added to Student

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,28 @@
 public class Student
 {
+    private String name;
+    private int age;
+    private double gpa;
 
+    public Student(String name, int age, double gpa)
+    {
+        this.name = name;
+        this.age = age;
+        this.gpa = gpa;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+
+    public double getGpa()
+    {
+        return gpa;
+    }
 }
