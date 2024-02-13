840353e6eaeb91db5e24629d9d0d8ad25d4ccefc
ijmrodriguez <ijonmrodriguez@gmail.com>
2024-02-12 16:09:08 -0800

    Added to LinkedListQueue

--- src/LinkedListQueue.java
+++ src/LinkedListQueue.java
@@ -1,4 +1,30 @@
-public class LinkedListQueue
+import java.util.LinkedList;
+public class LinkedListQueue<T>
 {
+    private LinkedList<T> queue;
 
+    public LinkedListQueue()
+    {
+        queue = new LinkedList<>();
+    }
+
+    public void enqueue(T item)
+    {
+        queue.addLast(item);
+    }
+
+    public T dequeue()
+    {
+        return queue.poll();
+    }
+
+    public boolean isEmpty()
+    {
+        return queue.isEmpty();
+    }
+
+    public int size()
+    {
+        return queue.size();
+    }
 }
