8e6e4e8752d69428a5c12bd5c2ebb1bfc9b66087
ijmrodriguez <ijonmrodriguez@gmail.com>
2024-02-13 12:50:31 -0800

    Changed the QueueLinkedList class.

---
+++ src/QueueLinkedList.java
@@ -0,0 +1,57 @@
+public class QueueLinkedList
+{
+    private Node front;
+    private Node rear;
+
+    public QueueLinkedList()
+    {
+        this.front = null;
+        this.rear = null;
+    }
+
+    public void enqueue(Student student)
+    {
+        Node newNode = new Node(student);
+        if(rear == null)
+        {
+            front = newNode;
+            rear = newNode;
+        }
+        else
+        {
+            rear.setNext(newNode);
+            rear = newNode;
+        }
+    }
+
+    public Student dequeue()
+    {
+        if(front == null)
+        {
+            throw new IllegalStateException("Queue is empty");
+        }
+        Student data = front.getData();
+        front = front.getNext();
+        if(front == null)
+        {
+            rear = null;
+        }
+        return data;
+    }
+
+    public boolean isEmpty()
+    {
+        return front == null;
+    }
+
+    public void display()
+    {
+        Node current = front;
+        while(current != null)
+        {
+            System.out.println("Name: " + current.getData().getName() + ", Age: " + current.getData().getAge() +
+                    ", GPA: " + current.getData().getGpa());
+            current = current.getNext();
+        }
+    }
+}
