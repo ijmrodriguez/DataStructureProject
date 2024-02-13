97e9472821f7a0fb9664aa0bd8132b0a0a1e89f5
ijmrodriguez <ijonmrodriguez@gmail.com>
2024-02-13 13:22:42 -0800

    Added a third test to QueueLinkedListTest

--- Tests/QueueLinkedListTest.java
+++ Tests/QueueLinkedListTest.java
@@ -1,4 +1,7 @@
 import org.junit.Test;
+
+import java.util.Queue;
+
 import static org.junit.Assert.*;
 
 public class QueueLinkedListTest
@@ -29,5 +32,22 @@ public class QueueLinkedListTest
         queue.dequeue();
         assertTrue(queue.isEmpty());
     }
-}
 
+    @Test //3
+    public void testEnqueueMultiple()
+    {
+        QueueLinkedList queue = new QueueLinkedList();
+        Student chase = new Student("Chase", 15, 3.5);
+        Student jordan = new Student("Jordan", 18, 2.5);
+        Student trisha = new Student("Trisha", 13, 4.0);
+
+        queue.enqueue(chase);
+        queue.enqueue(jordan);
+        queue.enqueue(trisha);
+
+        assertEquals(chase, queue.dequeue());
+        assertEquals(jordan, queue.dequeue());
+        assertEquals(jordan, queue.dequeue());
+        assertTrue(queue.isEmpty());
+    }
+}
