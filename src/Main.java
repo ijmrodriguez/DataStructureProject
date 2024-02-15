d72756d76ca07e0682665342cb700e68b987f253
ijmrodriguez <ijonmrodriguez@gmail.com>
2024-02-15 12:34:19 -0800

    Added output to main class

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        QueueLinkedList queue = new QueueLinkedList();
+        Student john = new Student("John", 13, 4.0);
+        Student alice = new Student("Alice", 16, 4.2);
+        Student ben = new Student("Ben", 14, 3.5);
+
+        queue.enqueue(john);
+        queue.enqueue(alice);
+        queue.enqueue(ben);
+
+        System.out.println("Data Structure Contains: ");
+        queue.display();
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -18,7 +18,7 @@ public class Student
 
     public void setName(String name)
     {
-        //this.name = name;
+        this.name = name;
     }
 
     public int getAge()
